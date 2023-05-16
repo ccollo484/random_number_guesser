d23b22ce1c09d1ed6500aaccc8d7a14148f3a090
ccollo484 <chriscollo2001@gmail.com>
2023-05-16 12:47:02 +0100

    add print message move if to bottom

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,14 +4,17 @@ import random
 
 python_number = random.randint(1, 100)
 
-user_input = input("What number is Python thinking of?: ")
-user_guess = int(user_input)
+while True:
+    print("What number between 1 and 100 is Python thinking of?")
+    user_input = input()
+    user_guess = int(user_input)
 
-if user_guess == python_number:
-    print(f"Well done! Python's number was {python_number}.")
+    if user_guess < python_number:
+        print("Your guess is lower than Python's number!")
 
-elif user_guess < python_number:
-    print("Your guess is lower than Python's number!")
-
-elif user_guess > python_number:
-    print("You guess is higher than pythons number")
+    elif user_guess > python_number:
+        print("You guess is higher than pythons number")
+    
+    elif user_guess == python_number:
+        print(f"Well done! Python's number was {python_number}.")
+    break
