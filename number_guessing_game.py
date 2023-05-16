be8d5ff463871dcaa060e5149d2a52c4afc980c2
ccollo484 <chriscollo2001@gmail.com>
2023-05-16 12:35:37 +0100

    add user_guess and add game code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,3 +4,13 @@ import random
 
 python_number = random.randint(1, 100)
 
+user_guess = input("What number is Python thinking of?: ")
+
+if user_guess == python_number:
+    print(f"Well done! Python's number was {python_number}.")
+
+elif user_guess < python_number:
+    print("Your guess is lower than Python's number!")
+
+elif user_guess > python_number:
+    print("You guess is higher than pythons number")
